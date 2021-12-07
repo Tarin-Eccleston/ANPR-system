09eb9c5045ea951eb3d5ae87e03bdb75310eb751
Tarin-Eccleston <tarin.eccleston@gmail.com>
2021-12-07 23:55:58 +1300

    add country

--- vehicle.py
+++ vehicle.py
@@ -2,14 +2,16 @@
 
 from _typeshed import Self
 import sqlite3
+from sqlite3.dbapi2 import register_adapter
 
 class Car:
-    def __init__(self, id, model, make, colour, number_plate):
+    def __init__(self, id, number_plate, model, make, colour, country):
         self.id = id
         self.number_plate = number_plate
         self.model = model
         self.make = make
         self.colour = colour
+        self.country = country
         self.verified = False
         
     def verificationResult(self, flag):
